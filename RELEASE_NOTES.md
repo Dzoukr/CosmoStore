3c9cfdae335dbbdf4cbf6e2b3a665dc2c2d0e056
Roman Provazník <dzoukr@dzoukr.cz>
2018-08-21 11:05:56 +0200

    Initial release with support for Cosmos DB

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,2 @@
+### 1.0.0 - August 20 2018
+* Initial release with support for Cosmos DB
--- build.fsx
+++ build.fsx
@@ -9,11 +9,20 @@ open Fake.Core.TargetOperators
 open Fake.DotNet
 open Fake.DotNet.Testing
 
-
 let appSrc = "src/CosmoStore"
+let testsSrc = "tests/CosmoStore.Tests"
+
+let package = "CosmoStore"
+let github = package
+let tags = "FSharp CosmosDB DocumentDB EventSourcing EventStore"
+let description = "Simple F# parser for Open API specification"
 
 Target.create "Build" (fun _ ->
     appSrc |> DotNet.build id
+)
+
+Target.create "RunTests" (fun _ ->
+    testsSrc |> DotNet.test id
 )
 
 Fake.Core.Target.create "Clean" (fun _ -> 
@@ -24,7 +33,35 @@ Fake.Core.Target.create "Clean" (fun _ ->
     |> Shell.deleteDirs
 )
 
+// Read release notes & version info from RELEASE_NOTES.md
+let release = ReleaseNotes.load "RELEASE_NOTES.md"
+
+Target.create "Nuget" (fun _ ->
+    let toNotes = List.map (fun x -> x + System.Environment.NewLine) >> List.fold (+) ""
+    let args = 
+        [
+            sprintf "PackageId=\"%s\"" package
+            sprintf "Title=\"%s\"" package
+            sprintf "Description=\"%s\"" description
+            sprintf "Summary=\"%s\"" description
+            sprintf "PackageVersion=\"%s\"" release.NugetVersion
+            sprintf "PackageReleaseNotes=\"%s\"" (release.Notes |> toNotes)
+            sprintf "PackageLicenseUrl=\"http://github.com/dzoukr/%s/blob/master/LICENSE.md\"" github
+            sprintf "PackageProjectUrl=\"http://github.com/dzoukr/%s\"" github
+            "PackageIconUrl=\"\""
+            sprintf "PackageTags=\"%s\"" tags
+            "Copyright=\"Roman Provazník - 2018\""
+            "Authors=\"Roman Provazník\""
+        ] 
+        |> List.map (fun x -> "/p:" + x)
+        |> String.concat " "
+
+    
+    appSrc |> DotNet.pack (fun p -> { p with Configuration = DotNet.Custom "Release"; OutputPath = Some "../../nuget"; Common = { p.Common with CustomParams = Some args } })
+)
+
 "Clean" ==> "Build"
+"RunTests" ==> "Clean" ==> "Nuget"
 
 // start build
 Fake.Core.Target.runOrDefault "Build"
